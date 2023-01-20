88491fcc026357ff27d4ca5d8de055cf7de983f9
Uzair Ansari <uzairans532@gmail.com>
2023-01-20 12:14:20 +0530

    day4

--- spice-nerve/src/Page/ProductDetails.jsx
+++ spice-nerve/src/Page/ProductDetails.jsx
@@ -33,7 +33,7 @@ export default function ProductDetails () {
                         src={
                             'https://images.unsplash.com/photo-1596516109370-29001ec8ec36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE1MDl8MHwxfGFsbHx8fHx8fHx8fDE2Mzg5MzY2MzE&ixlib=rb-1.2.1&q=80&w=1080'
                         }
-                        fit={'cover'}
+                    
                         align={'center'}
                         w={'100%'}
                         h={{ base: '100%', sm: '400px', lg: '500px' }}
