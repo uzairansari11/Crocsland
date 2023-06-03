a4eb86df4a658042c6ce0a09a739c1d6b8eead7e
uzairansari11 <uzairans532@gmail.com>
2023-06-03 21:53:01 +0530

    fixed loading indicator

--- spice-nerve/src/Component/Loading.jsx
+++ spice-nerve/src/Component/Loading.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Spinner } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import styles from "./Loading.module.css";
 
 export const Loading = () => {
   return (
@@ -9,21 +10,12 @@ export const Loading = () => {
       alignItems="center"
       height="100vh"
     >
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="red"
-        boxSize="100px"
-        zIndex="9999"
-        sx={{
-          borderRadius: "50%",
-          borderStyle: "dotted",
-          borderWidth: "8px",
-          animation: "spin 1s infinite linear",
-          margin: "8px",
-        }}
-      />
+      <Box display="flex" alignItems="center">
+        <Box className={styles["loading-dot"]} />
+        <Box className={styles["loading-dot"]} />
+        <Box className={styles["loading-dot"]} />
+
+      </Box>
     </Box>
   );
 };
